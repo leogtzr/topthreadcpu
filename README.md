992e823c44adf1576d9d120f4dfee7ff9379a16c
leogtzr <leogutierrezramirez@gmail.com>
2018-02-26 10:40:08 -0700

    Adding long options, refactor and adding output usage example.

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
 # Usage:
 
 ```
-./lwp_check.sh | less -R
+./lwp_check.sh [--stlen <STACKTRACE_LENGTH> --count <LWP_COUNT] | [--help|-h]
+```
+
+## Output
+
+```
+./lwp_check.sh --count 7 --stlen 14 | less -Ri
+```
+
+```
+Analyzing ===> {ATGWebServices01}, pid: 26721
+Top 7 lwps for ATGWebServices01 - 26721
+ 2.7 26721 26787  511 up000058  2.7 Feb23  2-18:03:09   511
+ 0.5 26721 26757  511 up000058  0.5 Feb23  2-18:03:11   511
+ 0.2 26721 26739  511 up000058  0.2 Feb23  2-18:03:11   511
+ 0.1 26721 26784  511 up000058  0.1 Feb23  2-18:03:09   511
+ 0.0 26721 28094  511 up000058  0.0 Feb23  2-17:54:32   511
+ 0.0 26721 28086  511 up000058  0.0 Feb23  2-17:54:39   511
+ 0.0 26721 27946  511 up000058  0.0 Feb23  2-17:56:04   511
+lwp: 26787 -> 0x68a3
+"dtmetricstimer" #22 daemon prio=5 os_prio=0 tid=0x00007fc0f4001000 nid=0x68a3 runnable [0x0000000000000000]
+   java.lang.Thread.State: RUNNABLE
+
+   Locked ownable synchronizers:
+        - None
+
+"dtcontroller" #21 daemon prio=5 os_prio=0 tid=0x00007fc110001000 nid=0x689e runnable [0x0000000000000000]
+   java.lang.Thread.State: RUNNABLE
+
+   Locked ownable synchronizers:
+        - None
+
+"dtautosensor" #20 daemon prio=5 os_prio=0 tid=0x00007fc10c001000 nid=0x689d runnable [0x0000000000000000]
+   java.lang.Thread.State: RUNNABLE
+
+lwp: 26757 -> 0x6885
+"VM Thread" os_prio=0 tid=0x00007fc20c861000 nid=0x6885 runnable
+
+"Gang worker#0 (Parallel GC Threads)" os_prio=0 tid=0x00007fc20c36e800 nid=0x6866 runnable
+
+"Gang worker#1 (Parallel GC Threads)" os_prio=0 tid=0x00007fc20c365800 nid=0x6867 runnable
+
+...
+
 ```
--- lwp_check.sh
+++ lwp_check.sh
@@ -1,23 +1,67 @@
 #!/bin/bash
 # Author: Leo Gutierrez | @leogtzr
-readonly MAX_NUMBER_LWP=15
-readonly STACKTRACE_LENGTH=50
+
+show_help() {
+cat <<HELP_TEXT
+
+    ${0} [--stlen <STACKTRACE_LENGTH> --count <LWP_COUNT] | [--help|-h]
+
+HELP_TEXT
+}
+
+readonly OPTS=$(getopt -o s:,c:,h --long stlen:,count:,help -- "${@}" 2> /dev/null)
+
+eval set -- "${OPTS}"
+stlen_flag=0
+count_flag=0
+
+while true; do
+    case "${1}" in
+
+        --stlen|-s)
+            stlen_flag=1
+            readonly STACKTRACE_LENGTH="${2}"
+            shift
+            shift
+            ;;
+
+        --count|-c)
+            date_flag=1
+            readonly MAX_NUMBER_LWP="${2}"
+            shift
+            shift
+            ;;
+
+        --help|-h)
+            show_help
+            exit 0
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
 
 for jvm_info in $(jps -l | awk '{print $1 "," $2}'); do
     jvmpid=$(awk -F ',' '{print $1}' <<< "${jvm_info}")
     instance=$(awk -F ',' '{print $2}' <<< "${jvm_info}")
     printf "Analyzing ===> {%s}, pid: %d\n" "${instance}" "${jvmpid}"
     if [[ ! -z "${jvmpid}" ]]; then
-        printf "Top %d lwps for %s - %d\n" "${MAX_NUMBER_LWP}" "${instance}" "${jvmpid}"
+        printf "Top %d lwps for %s - %d\n" "${MAX_NUMBER_LWP:-15}" "${instance}" "${jvmpid}"
         ps -eLo pcpu,pid,lwp,nlwp,ruser,pcpu,stime,etime,thcount | grep --fixed-strings "${jvmpid}" | \
-            sort --key 1 --reverse | head --lines ${MAX_NUMBER_LWP} > "lwps.${instance}"
+            sort --key 1 --reverse | head --lines ${MAX_NUMBER_LWP:-15} > "lwps.${instance}"
         if [[ -f "lwps.${instance}" ]]; then
             cat "lwps.${instance}"
             jstack -l "${jvmpid}" > "${instance}.${jvmpid}.tdump"
             while read lwp; do
                 pid_in_hex=$(printf "%x" "${lwp}")
                 printf "lwp: %s -> 0x%s\n" "${lwp}" "${pid_in_hex}"
-                LC_ALL=C grep --extended-regexp --after-context "${STACKTRACE_LENGTH}" " nid.*0x${pid_in_hex}" \
+                LC_ALL=C grep --extended-regexp --after-context "${STACKTRACE_LENGTH:-30}" " nid.*0x${pid_in_hex}" \
                     "${instance}.${jvmpid}.tdump" --color=always
             done < <(awk '{print $3}' "lwps.${instance}")
         fi
